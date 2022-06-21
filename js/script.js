97065cf0b09965abc2398b054b7437c73087e8d3
Roman Prilyuk <feraston@yandex.ru>
2022-06-22 02:42:42 +0300

    feat: add recenz slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // JS Сучилища
+var swiper1 = new Swiper(".mySwiper_one", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
