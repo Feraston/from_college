f76dd870f2d2413c124f89ae9b254700353d20e2
Roman Prilyuk <feraston@yandex.ru>
2022-06-22 14:57:20 +0300

    feat: add slider commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
-// JS Сучилища
-var swiper1 = new Swiper(".mySwiper_one", {
-  slidesPerView: 2,
+if(window.innerWidth >= 750){
+  var swiper1 = new Swiper(".mySwiper_one", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+} else {
+  var swiper1 = new Swiper(".mySwiper_one", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+
+var swiper2 = new Swiper(".mySwiper_two", {
+  // slidesPerView: 4,
   spaceBetween: 30,
-  centeredSlides: true,
   slidesPerView: 'auto',
   navigation: {
     nextEl: ".swiper-button-next",
@@ -13,3 +43,5 @@ var swiper1 = new Swiper(".mySwiper_one", {
     clickable: true,
   },
 });
+
+
